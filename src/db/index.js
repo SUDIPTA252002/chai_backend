a83ea36ecd1b837ec9b823ce0e8d1b787c2d1bea
SUDIPTA252002 <sudipta.mahato.ece25@heritageit.edu.in>
2024-01-07 21:36:47 +0530

    DB CONNECTION DONE

--- src/db/index.js
+++ src/db/index.js
@@ -3,11 +3,11 @@ import { DB_NAME } from "../constants.js";
 
 const dbconnect=async ()=>{
     try {
-        const connectioninstance=await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`)
+        const connectioninstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`MongoDB CONNECTED, DB HOST${connectioninstance.connection.host}`)
 
     } catch (error) {
-        console.log("ERROR:",error)
+        console.log("ERROR TMKC:",error)
         process.exit(1)
     }    
 }
