4cc148381f18033379785487853d1fe526e1bf15
SUDIPTA252002 <sudipta.mahato.ece25@heritageit.edu.in>
2024-01-10 11:51:06 +0530

    file upload suing multer

---
+++ src/utils/coludinaryFileUpload.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:process.env.CLOUDINARY_APIKEY, 
+    api_secret: process.env.CLOUDINARY_APIPASSWORD 
+  });
+
+
+  
+const fileUploaderCloudinary=async (filePath)=>{
+    try 
+    {
+        if(!filepath) return null
+
+        const response=await cloudinary.uploader.upload(filePath,{
+            resource_type:'auto'})
+         console.log('FILE HAS BEEN UPLOADED SUCCESSFULLY',response.url)
+
+    } catch (error) {
+        fs.unlinkSync(filePath)//removing the locally saved file as the upload got failed
+        return null
+    }
+}
+
+export {fileUploaderCloudinary}
